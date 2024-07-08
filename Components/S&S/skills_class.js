d6bd347de2f3bc56c759da73af80b01991b32ab3
DazedPenguin <harshucandor@gmail.com>
2024-07-08 21:18:10 +0530

    Added text in the homepage, Almost finished the skills/strengths section.

---
+++ Components/S&S/skills_class.js
@@ -0,0 +1,37 @@
+
+class Skills {
+
+  #skillName;
+  #skillDescription;
+
+  constructor(name, description) {
+
+    this.#skillName = name;
+    this.#skillDescription = description;
+    
+  }
+
+  getSkillName() {
+
+    return this.#skillName;
+  }
+  getSkillDescription() {
+
+    return this.#skillDescription;
+  }
+ 
+}
+
+
+function addSkillInputHTML () {
+
+  return (
+
+    'Hie'
+
+
+  )
+
+}
+
+
